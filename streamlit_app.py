9e3be76f71bc47922f8d830a4c1016e3a234d601
Tetradynamia <57911350+Tetradynamia@users.noreply.github.com>
2023-12-01 09:34:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,12 @@ def get_fruityvice_data(fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM fruit_load_list")
+    return my_cur.fetchall()
+    
+
 streamlit.title("My parents New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
@@ -43,16 +49,12 @@ except URLError as e:
   
 
   
+
+if streamlit.button("Get fruit_load_list")
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  streamlit.dataframe(get_fruit_load_list())
+
 streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.text("The fruit_load_list contains:")
-streamlit.dataframe(my_data_row)
-
-
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
 streamlit.write('Thanks for adding ', add_my_fruit)
 my_cur.execute("INSERT INTO fruit_load_list VALUES ('from streamlit')")
